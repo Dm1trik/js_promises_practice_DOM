15bef8e34e38e14a67089b2fd795fa32c7078817
Yevhenii Dmytriienko <dmitrik24042007@gmail.com>
2026-08-30 15:42:46 +0300

    fixed solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,13 @@
 'use strict';
 
 const firstPromise = new Promise((resolve, reject) => {
-  document.addEventListener('click', () => {
-    resolve('First promise was resolved');
-  });
+  document.addEventListener(
+    'click',
+    () => {
+      resolve('First promise was resolved');
+    },
+    { once: true },
+  );
 
   setTimeout(() => {
     reject(new Error('First promise was rejected'));
@@ -11,27 +15,43 @@ const firstPromise = new Promise((resolve, reject) => {
 });
 
 const secondPromise = new Promise((resolve, reject) => {
-  document.addEventListener('click', () => {
-    resolve('Second promise was resolved');
-  });
+  document.addEventListener(
+    'click',
+    () => {
+      resolve('Second promise was resolved');
+    },
+    { once: true },
+  );
 
-  document.addEventListener('contextmenu', (clickEvent) => {
-    clickEvent.preventDefault();
-    resolve('Second promise was resolved');
-  });
+  document.addEventListener(
+    'contextmenu',
+    (clickEvent) => {
+      clickEvent.preventDefault();
+      resolve('Second promise was resolved');
+    },
+    { once: true },
+  );
 });
 
 const leftClickPromise = new Promise((resolve) => {
-  document.addEventListener('click', () => {
-    resolve();
-  });
+  document.addEventListener(
+    'click',
+    () => {
+      resolve();
+    },
+    { once: true },
+  );
 });
 
 const rightClickPromise = new Promise((resolve) => {
-  document.addEventListener('contextmenu', (clickEvent) => {
-    clickEvent.preventDefault();
-    resolve();
-  });
+  document.addEventListener(
+    'contextmenu',
+    (clickEvent) => {
+      clickEvent.preventDefault();
+      resolve();
+    },
+    { once: true },
+  );
 });
 
 const thirdPromise = Promise.all([leftClickPromise, rightClickPromise]).then(
@@ -48,7 +68,7 @@ firstPromise
   .catch((error) => {
     document.body.insertAdjacentHTML(
       'beforeend',
-      `<div data-qa="notification" class="error">${error}</div>`,
+      `<div data-qa="notification" class="error">${error.message}</div>`,
     );
   });
 
@@ -62,7 +82,7 @@ secondPromise
   .catch((error) => {
     document.body.insertAdjacentHTML(
       'beforeend',
-      `<div data-qa="notification" class="error">${error}</div>`,
+      `<div data-qa="notification" class="error">${error.message}</div>`,
     );
   });
 
@@ -76,6 +96,6 @@ thirdPromise
   .catch((error) => {
     document.body.insertAdjacentHTML(
       'beforeend',
-      `<div data-qa="notification" class="error">${error}</div>`,
+      `<div data-qa="notification" class="error">${error.message}</div>`,
     );
   });
